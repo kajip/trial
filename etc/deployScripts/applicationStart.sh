cc59e7cddfbd3926230ffeb8540c6646a1b316af
Tomomi Kajita <t-kajita@biglobe.co.jp>
2017-10-24 11:43:57 +0900

    スクリプト修正
    Signed-off-by: Tomomi Kajita <t-kajita@biglobe.co.jp>

--- etc/deployScripts/applicationStart.sh
+++ etc/deployScripts/applicationStart.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-ln -s /etc/init.d/trial /home/ec2-user/trial-1.0.jar
-service trial stop
+
+ln -s /etc/init.d/trial /home/ec2-user/webapps/trial-1.0.jar
+service trial start
+chkconfig trial on
--- etc/deployScripts/applicationStop.sh
+++ etc/deployScripts/applicationStop.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+
 service trial stop
